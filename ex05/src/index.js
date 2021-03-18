959548aebc2d46099b8a32833b863b6f30e46089
farepule <faruk.puska@gmail.com>
2021-03-18 22:08:38 +0100

    NestedArray

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+//Only change code below this line
+var myNestedArray = [["Tablerone", 5], ["Milka", 3]]
+
+function myNestedFunction(arr) {
+  return myNestedArray;
+}
+
+//Only change code above this line
+
+console.log(myNestedFunction(myNestedArray));
+module.exports = myNestedFunction;
