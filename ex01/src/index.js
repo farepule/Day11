c7c4a3e1f9007b6467ebd8f085c12e913b905e81
farepule <faruk.puska@gmail.com>
2021-03-18 15:42:18 +0100

    array

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+//Create an array here
+myArr = [2, 4, 0, 8, 10];
+myArr[2] = 6;
+//End of creating an array
+function myArrayFunction(arr){
+    var myItems = [myArr];
+//Only change code below this line 
+
+    return myItems;
+//Only change code above this line 
+
+}
+
+console.log(myArrayFunction(myArr));
+module.exports = myArrayFunction;
